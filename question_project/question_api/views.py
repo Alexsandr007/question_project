d7cb4f0bed85a13b7c0de7587e832d648d67bb6a
Alex_djangp_projects <aleksone_01@mail.ru>
2025-09-09 12:11:12 +0300

    add serializers and views

--- question_project/question_api/views.py
+++ question_project/question_api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Question, Answer
+from .serializers import QuestionSerializer, AnswerSerializer
 
-# Create your views here.
+class QuestionListCreateView(generics.ListCreateAPIView):
+    queryset = Question.objects.all().order_by('-created_at')
+    serializer_class = QuestionSerializer
+
+class QuestionRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class AnswerCreateView(generics.CreateAPIView):
+    serializer_class = AnswerSerializer
+
+    def perform_create(self, serializer):
+        question_id = self.kwargs.get('question_id')
+        try:
+            question = Question.objects.get(pk=question_id)
+        except Question.DoesNotExist:
+            raise NotFound("Question not found")
+        serializer.save(question=question)
+
+class AnswerRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
