c02241c0f1de2824490eb5f62ecda5fb8edd1440
Alex_djangp_projects <aleksone_01@mail.ru>
2025-09-09 12:08:27 +0300

    added checks for empty fields

--- question_project/question_api/models.py
+++ question_project/question_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Question(models.Model):
+    text = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    user_id = models.UUIDField()
+    text = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
